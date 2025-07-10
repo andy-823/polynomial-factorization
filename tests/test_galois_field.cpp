82e128bef851ad4335005d1b65a336ad2be55fb3
Ishutin Andrei <andy-in@mail.ru>
2025-07-10 10:50:00 +0300

    Add tests for log based field

--- tests/test_galois_field.cpp
+++ tests/test_galois_field.cpp
@@ -5,8 +5,9 @@
 
 #include <factorization/concepts.hpp>
 #include <factorization/galois_field/log_based_field.hpp>
+#include <factorization/galois_field/galois_field_element.hpp>
 
-using namespace factorization::internal;  // NOLINT
+using namespace factorization;  // NOLINT
 
 enum QueryType { kAdd, kNegative, kMultiply, kInverse, kPow };
 
@@ -30,7 +31,7 @@ std::vector<Test<NewInt>> TransformTests(const std::vector<OldInt>& tests) {
   return result;
 }
 
-template <GaloisField GaloisField, typename Int>
+template <concepts::GaloisField GaloisField, typename Int>
 void RunTests(const std::vector<Test<Int>>& tests) {
   GaloisField field;
 
@@ -159,12 +160,70 @@ TEST_CASE("LogBaseGaloisField") {
       {QueryType::kPow, 2, 7, 1},
     };
 
-    using GaloisField = LogBasedField<2, 3, {1, 1, 0, 1}>;
+    using GaloisField = galois_field::LogBasedField<2, 3, {1, 1, 0, 1}>;
     RunTests<GaloisField>(tests);
   }
 
   SECTION("GF9") {
+    std::vector<Test<int64_t>> tests = {
+      {QueryType::kMultiply, 0, 0, 0},
+      {QueryType::kMultiply, 0, 1, 0},
+      {QueryType::kMultiply, 0, 2, 0},
+      {QueryType::kMultiply, 0, 3, 0},
+      {QueryType::kMultiply, 0, 4, 0},
+      {QueryType::kMultiply, 0, 5, 0},
+      {QueryType::kMultiply, 0, 6, 0},
+      {QueryType::kMultiply, 0, 7, 0},
+      {QueryType::kMultiply, 0, 8, 0},
 
+      {QueryType::kMultiply, 1, 0, 0},
+      {QueryType::kMultiply, 1, 1, 1},
+      {QueryType::kMultiply, 1, 2, 2},
+      {QueryType::kMultiply, 1, 3, 3},
+      {QueryType::kMultiply, 1, 4, 4},
+      {QueryType::kMultiply, 1, 5, 5},
+      {QueryType::kMultiply, 1, 6, 6},
+      {QueryType::kMultiply, 1, 7, 7},
+      {QueryType::kMultiply, 1, 8, 8},
 
+      {QueryType::kMultiply, 2, 0, 0},
+      {QueryType::kMultiply, 2, 1, 2},
+      {QueryType::kMultiply, 2, 2, 1},
+      {QueryType::kMultiply, 2, 3, 6},
+      {QueryType::kMultiply, 2, 4, 8},
+      {QueryType::kMultiply, 2, 5, 7},
+      {QueryType::kMultiply, 2, 6, 3},
+      {QueryType::kMultiply, 2, 7, 5},
+      {QueryType::kMultiply, 2, 8, 4},
+
+      {QueryType::kMultiply, 3, 0, 0},
+      {QueryType::kMultiply, 3, 1, 3},
+      {QueryType::kMultiply, 3, 2, 6},
+      // x * x = x + 1
+      {QueryType::kMultiply, 3, 3, 4},
+      // x * (x + 1) = x^2 + x = 2x + 1
+      {QueryType::kMultiply, 3, 4, 7},
+      // x * (x + 2) = x^2 + 2x = 1
+      {QueryType::kMultiply, 3, 5, 1},
+      // x * (2x) = 2x^2 = 2x + 2
+      {QueryType::kMultiply, 3, 6, 8},
+      // x * (2x + 1) = 2x + 2 + x = 2
+      {QueryType::kMultiply, 3, 7, 2},
+      // x * (2x + 2) = 2x + 2 + 2x = x + 2
+      {QueryType::kMultiply, 3, 8, 5},
+
+      {QueryType::kAdd, 2, 0, 2},
+      {QueryType::kAdd, 3, 1, 4},
+      {QueryType::kAdd, 5, 2, 4},
+      {QueryType::kAdd, 7, 3, 1},
+      {QueryType::kAdd, 8, 4, 0},
+      {QueryType::kAdd, 4, 5, 6},
+      {QueryType::kAdd, 2, 6, 8},
+      {QueryType::kAdd, 3, 7, 1},
+      {QueryType::kAdd, 1, 8, 6},
+    };
+    // x^2 = x + 1
+    using GaloisField = galois_field::LogBasedField<3, 2, {2, 2, 1}>;
+    RunTests<GaloisField>(tests);
   }
 }
