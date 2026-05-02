9e257fe7986db0506fc240d05f0045c79b424c79
Andy <andy@192.168.1.3>
2026-05-03 02:21:47 +0300

    Add new experiment

---
+++ experiments/experiment_3/experiment.cpp
@@ -0,0 +1,151 @@
+#include <atomic>
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <random>
+#include <string>
+
+#include <factorization/galois_field/log_based_field.hpp>
+#include <factorization/galois_field/field_element_wrapper.hpp>
+#include <factorization/polynomial/simple_polynomial.hpp>
+#include <factorization/parallel/thread_pool.hpp>
+#include <factorization/parallel/wait_group.hpp>
+#include <factorization/solver/berlekamp.hpp>
+#include <factorization/concepts.hpp>
+#include <factorization/utils.hpp>
+
+#include "berlekamp.hpp"
+#include "generator.hpp"
+
+using namespace factorization;  // NOLINT
+
+struct ExperimentParams {
+  size_t test_value;
+  size_t thread_count;
+  size_t test_runs;
+};
+
+template <typename RandomGen>
+class MultithreadRandomGen {
+ public:
+  using result_type = typename RandomGen::result_type;
+
+  void seed(result_type seed) {
+    lock_.lock();
+    gen_.seed(seed);
+    lock_.unlock();
+  }
+
+  result_type operator()() {
+    lock_.lock();
+    result_type result = gen_();
+    lock_.unlock();
+    return result;
+  }
+
+ private:
+  RandomGen gen_;
+  std::mutex lock_;
+};
+
+template <concepts::GaloisField Field, typename RandomGen>
+void RunExperiment(std::ostream& out, const ExperimentParams& params, RandomGen& gen) {
+  using Element = galois_field::FieldElementWrapper<Field>;
+  using Poly = polynomial::SimplePolynomial<Element>;
+
+  constexpr int kFieldSize = utils::BinPow(Field::FieldBase(), Field::FieldPower());
+
+  parallel::ThreadPool runtime(params.thread_count);
+  parallel::WaitGroup wg;
+  runtime.Start();
+
+  std::vector<std::atomic<int64_t>> counts(params.test_value + 1);
+
+  out << kFieldSize << "\t\t";
+  wg.Add(params.test_runs);
+  for (size_t test = 0; test < params.test_runs; ++test) {
+    parallel::SubmitTask(&runtime, [&]{    
+      solver::BerlekampExperiment<Poly> solver;
+
+      RandomGen local_gen;
+      local_gen.seed(gen());
+
+      Poly poly = GenPoly<Poly>(local_gen, params.test_value);
+      for (const auto& [factor, power] : solver.Factorize(poly)) {
+        counts[factor.Size() - 1].fetch_add(1);
+      }
+      wg.Done();
+    });
+  }
+  wg.Wait();
+  for (const auto& count : counts) {
+    double avg = static_cast<double>(count.load());
+    out << std::setprecision(3) << std::fixed << avg / params.test_runs  << "\t";
+  }
+  out << "\n";
+
+  runtime.Stop();
+}
+
+int main() {
+  // https://www.partow.net/programming/polynomials/index.html
+  using GF2_1 = galois_field::LogBasedField<2, 1, {1, 1}>;
+  using GF2_2 = galois_field::LogBasedField<2, 2, {1, 1, 1}>;
+  using GF2_3 = galois_field::LogBasedField<2, 3, {1, 1, 0 , 1}>;
+  using GF2_4 = galois_field::LogBasedField<2, 4, {1, 1, 0, 0, 1}>;
+  using GF2_5 = galois_field::LogBasedField<2, 5, {1, 0, 1, 0, 0, 1}>;
+  using GF2_6 = galois_field::LogBasedField<2, 6, {1, 1, 0, 0, 0, 0, 1}>;
+  using GF2_7 = galois_field::LogBasedField<2, 7, {1, 1, 0, 0, 0, 0, 0, 1}>;
+  using GF2_8 = galois_field::LogBasedField<2, 8, {1, 0, 1, 1, 1, 0, 0, 0, 1}>;
+  using GF2_9 = galois_field::LogBasedField<2, 9, {1, 0, 0, 0, 1, 0, 0, 0, 0, 1}>;
+  using GF2_10 = galois_field::LogBasedField<2, 10, {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1}>;
+  using GF2_16 = galois_field::LogBasedField<2, 16, { 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1 }>;
+
+
+  constexpr int kRuns = 1'000;
+  constexpr int kTestValue = 400;
+  constexpr int kThreads = 12; //20;
+  const std::string path{"../../experiments/experiment_3/exp_3_out.txt"};
+
+  ExperimentParams params;
+  params.test_value = kTestValue;
+  params.thread_count = kThreads;
+  params.test_runs = kRuns;
+
+  std::ofstream out_file;
+  out_file.open(path);
+  if (!out_file.is_open()) {
+    std::cout << "bad\n";
+    return 0;
+  }
+  std::ostream& out = out_file; //std::cout;
+
+  out << "\t\t\t";
+  for (size_t i = 0; i <= kTestValue; ++i) {
+    out << i << "\t\t";
+  }
+  out << "\n";
+
+  MultithreadRandomGen<std::mt19937> gen;
+  gen.seed(0);
+
+  // out << "\n";
+  gen.seed(0);
+
+  RunExperiment<GF2_1>(out, params, gen);
+  RunExperiment<GF2_2>(out, params, gen);
+  // RunExperiment<GF2_3>(out, params, gen);
+  RunExperiment<GF2_4>(out, params, gen);
+  // RunExperiment<GF2_5>(out, params, gen);
+  // RunExperiment<GF2_6>(out, params, gen);
+  // RunExperiment<GF2_7>(out, params, gen);
+  RunExperiment<GF2_8>(out, params, gen);
+  // RunExperiment<GF2_9>(out, params, gen);
+  RunExperiment<GF2_16>(out, params, gen);
+
+  out_file.close();
+
+  return 0;
+}
---
+++ experiments/experiment_3/generator.hpp
@@ -0,0 +1,37 @@
+#include <factorization/concepts.hpp>
+#include <factorization/utils.hpp>
+
+namespace factorization {
+
+template <concepts::GaloisFieldElement Element>
+constexpr auto AllElements() {
+  constexpr int kFieldSize = utils::BinPow(Element::FieldBase(),
+                                           Element::FieldPower());
+  auto elements_range = Element::AllFieldElements();
+  std::array<Element, kFieldSize> result{};
+  std::copy(elements_range.begin(), elements_range.end(), result.begin());
+  return result;
+}
+
+template <concepts::GaloisFieldElement Element, typename RandomGen>
+Element GenElement(RandomGen& gen) {
+  // TODO: use std discrete distribution
+  // assume field to be relatively small
+  constexpr static auto elements = AllElements<Element>();
+  size_t index = gen() %  elements.size();
+  return elements[index];
+}
+
+template <concepts::Polynom Poly, typename RandomGen>
+Poly GenPoly(RandomGen& gen, size_t size) {
+  using Element = typename Poly::Element;
+  
+  std::vector<Element> elements(size + 1);
+  for (auto& element : elements) {
+    element = GenElement<Element>(gen);
+  }
+  elements.back() = Element::One();
+  return Poly(std::move(elements));
+}
+
+}  // namespace factoriation
